e6bafc7ca2714cd894885849ec2695b0e7bf932a
arskell <arskell221@gmail.com>
2022-11-05 18:57:57 +0300

    rm point_light

--- include/enbine/graphics/scene/light/point_light.h
+++
@@ -1,8 +0,0 @@
-#ifndef POINT_LIGHT_H
-#define POINT_LIGHT_H
-#include "bright_element.h"
-#include "../primitives/primitive_object.h"
-
-class PointLight: public BrightElement, public PrimitiveObject{};
-
-#endif
