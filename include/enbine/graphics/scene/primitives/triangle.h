ca19a9a67af0c3c2a392bbb9b4af8e02179d3ab4
arskell <arskell221@gmail.com>
2022-11-05 18:59:53 +0300

    triangle inherits from intersectable object

--- include/enbine/graphics/scene/primitives/triangle.h
+++ include/enbine/graphics/scene/primitives/triangle.h
@@ -5,9 +5,10 @@
 #include "../../math_unit/vec3.h"
 #include "../../ray/ray.h"
 #include "../../ray/intersection_info.h"
-#include "primitive_object.h"
+#include "intersectable_object.h"
 
-class Triangle: public PrimitiveObject{
+
+class Triangle: public IntersectableObject{
 private:
     Vec3 _a;
     Vec3 _b;
@@ -29,8 +30,14 @@ public:
     Vec3 get_position() const override;
     void set_n(Vec3 n) override;
     Vec3 get_n() const override{return _plane.n;}
-
+   
+    //create "intersected" class and move it there (maybe not. need to create special method)
     friend IntersectionInfo calc_intersection(const Triangle& triange, const Ray& ray);
+     
+    IntersectionInfo get_intersection(const Ray& ray) const override{
+        return calc_intersection(*this, ray);
+    }
+
 };
 
 
