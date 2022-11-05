f111f560e196511b5035ab5fbb0fcae1c9bf59d1
arskell <arskell221@gmail.com>
2022-11-05 18:58:08 +0300

    create light

---
+++ include/enbine/graphics/scene/light/light.h
@@ -0,0 +1,10 @@
+#ifndef LIGHT_H
+#define LIGHT_H
+#include "bright_element.h"
+#include "../primitives/primitive_object.h"
+
+class Light: public BrightElement, public PrimitiveObject{
+    //get flux by vector
+};
+
+#endif
