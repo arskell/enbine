9c6e65e36000d0cd7d336fe85a6cc93d77590d98
arskell <arskell221@gmail.com>
2022-11-05 21:29:09 +0300

    illuminance in point light

---
+++ include/enbine/graphics/scene/light/point_light.h
@@ -0,0 +1,23 @@
+#ifndef POINT_LIGHT_H
+#define POINT_LIGHT_H
+#include "light.h"
+#include "../../math_unit/vec3.h"
+#include <math.h>
+
+class PointLight: public Light{
+    public:
+    LightComponentT get_illumination(const Vec3& n, const Vec3& p) const override{
+
+        auto len_mul = length(n) * length(_n);
+
+        auto cos_alpha = abs((n * _n)/(len_mul));
+        
+        auto intensity = get_flux()/(M_PI * 4);
+
+        auto r2 = distance2(p, this->get_position());
+
+        return (intensity * cos_alpha) / r2;
+    }
+};
+
+#endif
