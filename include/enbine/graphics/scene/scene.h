0c7e53fc8c95dfb791c34770e0cff876f14952a5
arskell <arskell221@gmail.com>
2022-12-20 04:01:29 +0300

    shadows. And fixed bug

--- include/enbine/graphics/scene/scene.h
+++ include/enbine/graphics/scene/scene.h
@@ -9,8 +9,14 @@
 
 class Scene{
     private:
+
+    const LightComponentT _background = {0, 0, 0};
+
     std::map<std::string, std::shared_ptr<Light>> _light;
     std::map<std::string, std::shared_ptr<IntersectableObject>> _objects;
+
+    IntersectionInfo _get_shortest_intersection(const Ray& ray) const;
+
     public:
     void add(const std::string& name, std::shared_ptr<Light> light){_light.insert({name, light});}
     void add(const std::string& name, std::shared_ptr<IntersectableObject> obj){ _objects.insert({name, obj});};
--- src/graphics/scene/scene.cpp
+++ src/graphics/scene/scene.cpp
@@ -1,35 +1,51 @@
 #include "enbine/graphics/scene/scene.h"
 #include <vector>
+#include <iostream>
 
-LightComponentT Scene::get_light(const Ray& ray){
-    const LightComponentT background = {0, 0, 0};
 
-    if(_objects.empty()){return background;}
+ IntersectionInfo Scene::_get_shortest_intersection(const Ray& ray) const{
+    if(_objects.empty()){return {};}
+
     IntersectionInfo shortest = _objects.begin()->second->get_intersection(ray);
     
     for(auto it = std::next(_objects.begin()); it != _objects.end(); ++it){
         auto intersection =  it->second->get_intersection(ray);
     
         if(intersection.is_intersected){
-            if(shortest.t > intersection.t){
+            if(!shortest.is_intersected || shortest.t > intersection.t){
                 shortest = intersection;
             }
         }
     }
 
-    if(!shortest.is_intersected){
-        return background;
-    }
+    return shortest;
+ }
 
+LightComponentT Scene::get_light(const Ray& ray){
     
+    auto shortest = _get_shortest_intersection(ray);
+    if(!shortest.is_intersected){
+        return _background;
+    }
 
     LightComponentT l_global{};
     auto p = to_vec3(ray, shortest.t);
     auto material_coeff = shortest.material->get_material_light_coefficients(p, ray.d, {/*todo calculate reflection vector*/});
 
     for(const auto& l: _light){
-
-        //todo get shadow here
+        //test shadow here
+        //calulate distance to light
+        auto min_dist = distance(l.second->get_position(), p);
+        //contruct ray 
+        Ray ray_to_light;
+        ray_to_light.d = normalize(l.second->get_position() - p);
+        ray_to_light.p = p + ray_to_light.d * 10; /*< some step out (to avoid false positive intersecting with itself object)*/
+        auto ray_to_light_intersection = _get_shortest_intersection(ray_to_light);
+        //test
+        if(ray_to_light_intersection.is_intersected && ray_to_light_intersection.t < min_dist){
+            //failed
+            break;
+        }
         auto curr_illum = l.second->get_illumination(shortest.n, p);
         auto l_current =  LightComponentT{curr_illum.x1 * material_coeff.x1, curr_illum.x2 * material_coeff.x2, curr_illum.x3 * material_coeff.x3};
         l_global = l_global + l_current;
