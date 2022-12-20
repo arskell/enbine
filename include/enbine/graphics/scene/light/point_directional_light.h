0e942bc313d8825cd0a7c739577f9ec7c0daab94
arskell <arskell221@gmail.com>
2022-12-21 00:36:21 +0300

    New light type

--- include/enbine/graphics/scene/light/point_directional_light.h
+++ include/enbine/graphics/scene/light/point_directional_light.h
@@ -1,20 +1,22 @@
-#ifndef POINT_LIGHT_H
-#define POINT_LIGHT_H
+#ifndef POINT_DIRECTIONAL_LIGHT_H
+#define POINT_DIRECTIONAL_LIGHT_H
 #include "light.h"
 #include "../../math_unit/vec3.h"
 #include <math.h>
 
-class PointLight: public Light{
+class PointDirectinalLight: public Light{
     public:
     LightComponentT get_illumination(const Vec3& n, const Vec3& p) const override{
 
         auto len_mul = length(n) * length(_n);
 
+        auto cos_alpha = abs((n * _n)/(len_mul));
+        
         auto intensity = get_flux()/(M_PI * 4);
 
         auto r2 = distance2(p, this->get_position());
         
-        auto illumination = intensity / r2;
+        auto illumination = (intensity * cos_alpha) / r2;
 
         return illumination;
     }
