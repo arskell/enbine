a8cfa38223216472ee4415e191a6c2604613a6f7
arskell <arskell221@gmail.com>
2022-12-20 04:02:09 +0300

    custom color on metal material

--- include/enbine/graphics/material/metal.h
+++ include/enbine/graphics/material/metal.h
@@ -8,8 +8,10 @@
 
 class MetalMaterial: public Material{
     Diffuse _reflectness;
-    const RGB _primary_color{0.05f, 0.5f, 0.05f};    
+    const RGB _primary_color{0.5f, 0.5f, 0.5f};    
     public:
+    MetalMaterial(){}
+    MetalMaterial(RGB primary_color):_primary_color(primary_color){}
     LightComponentT get_material_light_coefficients(const Vec3& p, const Vec3& wi, const Vec3& w0) override{
         auto ref = _reflectness.get_light_coefficients(p, wi, w0);
         return  {_primary_color.r * ref.x1, _primary_color.g * ref.x2, _primary_color.b * ref.x3};
